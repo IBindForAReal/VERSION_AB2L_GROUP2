bfbf68db230752286a1227a8c591ef6a81a443cc
Tin Caracuel <tincaracuel@gmail.com>
2013-03-03 12:18:25 +0800

    documentation of cashierManager & cashierAccess

--- IDSSystem/application/controllers/cashierManager.php
+++ IDSSystem/application/controllers/cashierManager.php
@@ -7,11 +7,15 @@ public function index(){
 	$this->load->view('cashier');
 }
 
+
 public function addCashier(){
+	/*gets the necessary information from the submitted form*/
 	$name = $_POST['cashierName'];
 	$username = $_POST['cashierUsername'];
 	$password = $_POST['cashierPassword'];
 
+	/*	calls the addNewCashier function in cashierAccess.php (in models) to add the new account
+	and it will return a status which will determine if the new account was successfully added */
 	$status = $this->cashierAccess->addNewCashier($name, $username, $password);
 	if($status == ''){
 		$this->load->view("message", array('message'=> $name.' has been added as a cashier.'));
@@ -21,21 +25,28 @@ public function addCashier(){
 }
 
 public function obtainCashiers(){
+	/* 	calls the getAllCashiers function in cashierAccess.php (in models) to
+		display the names of all cashier for selection (drop down menu) */
 		$res = $this->cashierAccess->getAllCashiers();
 		$this->load->view('listOfCashiers', array('cashiers'=>$res));
 	}
 
 public function obtainCashierDetails(){
+	/* 	calls the getCashierDetails function in cashierAccess.php (in models) to
+		view all the details of the cashierName chosen by the user */
 		$name = $_POST['cashierName'];
 		$res = $this->cashierAccess->getCashierDetails($name);
 		$this->load->view('cashierDetails', array('details'=>$res));
 	}
 
 public function editCashier(){
+	/*gets the necessary information from the submitted form*/
 	$name = $_POST['cashierName'];
 	$username = $_POST['cashierUsername'];
 	$password = $_POST['cashierPassword'];
 
+	/*	calls the editExistingCashier function in cashierAccess.php (in models) to edit the account
+	and it will return a status which will determine if the account was successfully edited */	
 	$status = $this->cashierAccess->editExistingCashier($name, $username, $password);
 	if($status === ''){
 		$this->load->view("message", array('message'=> $name.' has been edited.'));
@@ -45,8 +56,11 @@ public function editCashier(){
 }
 
 public function deleteCashier(){
+	/* this is what the user selected from the list of cashiers */
 	$name = $_POST['cashierName'];
 
+	/*	calls the deleteExistingCashier function in cashierAccess.php (in models) to delete the account
+	and it will return a status which will determine if the account was successfully deleted */
 	$status = $this->cashierAccess->deleteExistingCashier($name);
 	if($status === ''){
 		$this->load->view("message", array('message'=> $name.' has been deleted.'));
--- IDSSystem/application/models/cashierAccess.php
+++ IDSSystem/application/models/cashierAccess.php
@@ -1,47 +1,53 @@
 <?php
 class cashierAccess extends CI_Model {
+
+	/*	queries for the database involving the accounts of the cashier 	*/
+
+	/*	adds a new cashier to the system, it will return '' or the error number
+		to determine if the operations was successful */
 	function addNewCashier($name, $username, $password){
 		$status = $this->db->query("INSERT into cashier (cashier_name, cashier_username, cashier_password) values ('$name', '$username', '$password')");
 		if(!$status){
 			$this->error = $this->db->_error_message();
     		$this->errorno = $this->db->_error_number();
 
-    		//echo "error: " . $this->error;
-    		//echo "error no: " . $this->errorno;
     		return $this->error;
 		}
 		return '';
 	}
 
 	function getAllCashiers(){
+		/* simply displays the names of all cashier for selection (drop down menu) */
 		return $this->db->query("SELECT cashier_name from cashier")->result_array();
 	}
 
 	function getCashierDetails($name){
+		/* 	given the variable name, this function will select all the details whose cashier_name
+			matches the variable $name */
 		return $this->db->query("SELECT * from cashier where cashier_name='$name'")->result_array();
 	}
 
 	function editExistingCashier($name, $username, $password){
+		/* 	given the name, username and password, this function will update/edit all the details
+			whose cashier_name matches the variable $name */
 		$status = $this->db->query("UPDATE cashier set cashier_username='$username', cashier_password='$password' where cashier_name='$name'");
 		if(!$status){
 			$this->error = $this->db->_error_message();
     		$this->errorno = $this->db->_error_number();
 
-    		//echo "error: " . $this->error;
-    		//echo "error no: " . $this->errorno;
     		return $this->error;
 		}
 		return '';
 	}
 
 	function deleteExistingCashier($name){
+		/* 	given the variable name, this function will delete all the details whose cashier_name
+			matches the variable $name */
 		$status = $this->db->query("DELETE from cashier where cashier_name='$name'");
 		if(!$status){
 			$this->error = $this->db->_error_message();
     		$this->errorno = $this->db->_error_number();
 
-    		//echo "error: " . $this->error;
-    		//echo "error no: " . $this->errorno;
     		return $this->error;
 		}
 		return '';
